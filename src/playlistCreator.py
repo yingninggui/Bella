c9d359c4dfe4c9cb22082f62d9f5de949f5419b2
Yingning Gui <yingning.gui@uwaterloo.ca>
2017-11-18 15:47:49 -0500

    cleaning files

---
+++ src/playlistCreator.py
@@ -0,0 +1,54 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials 
+from spotipy.client import Spotify
+
+SPOTIPY_CLIENT_ID = 'eaef1e6ac22344f181edd44e36a48863'
+SPOTIPY_CLIENT_SECRET = '9b8ae1b5beed43bc9b210ac263b327ba'
+
+mySpotipyClient = Spotify(object)
+mySpotipy = SpotifyClientCredentials()
+mySpotipy.init(self, SPOTIPY_ClIENT_ID, SPOTIPY_CLIENT_SECRET, None)
+
+mySpotipy.get_access_token(self)
+
+
+if (case==1):
+    starter = sp.search(q=genre)
+    
+else if(case==2):
+    starter = sp.search(q=genre)
+    
+else if(case==3):
+    starter = sp.search(q=genre)
+    
+else if(case==4):
+    starter = sp.search(q=genre)
+    
+else if(case==5):
+    starter = sp.search(q=genre)
+    
+else if(case==6):
+    starter = sp.search(q=genre)
+    
+else if(case==7):
+    starter = sp.search(q=genre)
+    
+else if(case==8):
+    starter = sp.search(q=genre)
+    
+else if(case==9):
+    starter = sp.search(q=genre)
+    
+else: break; 
+
+#take category and find a seed song
+#add music, 9 switch statements
+
+
+if 
+
+#search
+#through songs 
+#add to playlist
+#repeat
+
